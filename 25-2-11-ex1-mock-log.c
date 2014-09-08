610bf8732d06032ec21ac0980e5305243b7d747d
Durrrr <ihaku4@gmail.com>
2014-09-08 23:29:20 +0800

    buggy ini-to-xml; time function test; note.

--- 25-2-11-ex1-mock-log.c
+++ 25-2-11-ex1-mock-log.c
@@ -2,24 +2,64 @@
 #include <stdlib.h>
 #include <time.h>
 
+void test_func();
+
 int main(void)
 {
   FILE *fp;
   int ch;
+  int is_loop = 0;
+
+  test_func();
 
   if ( (fp = fopen("mock_log_file.log", "a")) == NULL) {
     perror("Open file mock_log_file.log\n");
     exit(1);
   }
 
-  while (1) {
+  while (is_loop) {
     sleep(1);
     time_t t = time(NULL);
-    char *t_str = asctime(localtime(&t));
-    //char *t_str = asctime(localtime(&(time(NULL))));
-    fputs( t_str, fp);
+    struct tm *local_time = localtime(&t);
+    char *t_str = asctime(local_time);
+    //char *t_str = asctime(localtime(&t));
+    //char *t_str = asctime(localtime(&(time(NULL)))); // this not work.
+    //fputs( t_str, fp);
+    fputs( t_str, stdout);
     fflush(NULL);
   }
   fclose(fp);
   return 0;
 }
+
+void test_func() {
+  printf("\n%d", sizeof(struct tm));
+  printf("\n%d", sizeof(time_t));
+  time_t t = time(NULL);
+  print_struct_tm(&t);
+}
+
+void print_struct_tm(const struct tm *time) {
+  printf("tm_sec: %x\n", time->tm_sec);
+  printf("tm_min: %x\n", time->tm_min);
+  printf("tm_hour: %x\n", time->tm_hour);
+  printf("tm_mday: %x\n", time->tm_mday);
+  printf("tm_mon: %x\n", time->tm_mon);
+  printf("tm_year: %x\n", time->tm_year);
+  printf("tm_wday: %x\n", time->tm_wday);
+  printf("tm_yday: %x\n", time->tm_yday);
+  printf("tm_isdst: %x\n", time->tm_isdst);
+}
+
+//struct tm {
+//  int tm_sec;         /* seconds */
+//  int tm_min;         /* minutes */
+//  int tm_hour;        /* hours */
+//  int tm_mday;        /* day of the month */
+//  int tm_mon;         /* month */
+//  int tm_year;        /* year */
+//  int tm_wday;        /* day of the week */
+//  int tm_yday;        /* day in the year */
+//  int tm_isdst;       /* daylight saving time */
+//};
+//
